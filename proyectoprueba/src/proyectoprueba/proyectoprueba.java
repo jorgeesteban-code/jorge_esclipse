d740ec4b30588ed9910b86fe4cc03a0bc927685d
jorgeesteban-code <jorgeesteban2493@gmail.com>
2024-12-19 15:28:07 +0100

    Update proyectoprueba.java

--- proyectoprueba/src/proyectoprueba/proyectoprueba.java
+++ proyectoprueba/src/proyectoprueba/proyectoprueba.java
@@ -4,6 +4,8 @@ public class proyectoprueba {
 
 	public static void main(String[] args) {
 		System.out.println("Hola wankenoobi");
+		System.out.println("Agur yogur");
+		System.out.println("Ciao Pescao");
 
 	}
 
